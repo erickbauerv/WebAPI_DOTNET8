8eacb52307e785a363a6479866a546caab3f34e5
erickbauer <contatoerickbauer@gmail.com>
2025-03-17 18:48:23 -0300

    Implementing ListAuthors endpoint

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI_DOTNET8.Models;
+using WebAPI_DOTNET8.Services.Author;
 
 namespace WebAPI_DOTNET8.Controllers
 {
@@ -7,6 +9,19 @@ namespace WebAPI_DOTNET8.Controllers
     [ApiController]
     public class AuthorController : ControllerBase
     {
+        private readonly IAuthorInterface _authorInterface;
+
+        public AuthorController(IAuthorInterface authorInterface)
+        {
+            _authorInterface = authorInterface;
+        }
+
+        [HttpGet("ListAuthors")]
+        public async Task<ActionResult<ResponseModel<List<AuthorModel>>>> ListAuthors()
+        {
+            var authors = await _authorInterface.ListAuthors();
+            return Ok(authors);
+        }
 
     }
 }
