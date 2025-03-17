860c67365604f03ab6be36ff0e32275e09118dce
erickbauer <contatoerickbauer@gmail.com>
2025-03-17 18:34:28 -0300

    Implementation of the ListAuthors method

--- Services/Author/AuthorService.cs
+++ Services/Author/AuthorService.cs
@@ -1,7 +1,44 @@
-﻿namespace WebAPI_DOTNET8.Services.Author
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_DOTNET8.Data;
+using WebAPI_DOTNET8.Models;
+
+namespace WebAPI_DOTNET8.Services.Author
 {
     public class AuthorService : IAuthorInterface
     {
+        private readonly AppDbContext _context;
+        public AuthorService(AppDbContext context)
+        {
+            _context = context;
+        }
 
+        public Task<ResponseModel<AuthorModel>> GetAuthorByBookId(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<AuthorModel>> GetAuthorById(int idAuthor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ResponseModel<List<AuthorModel>>> ListAuthors()
+        {
+            ResponseModel<List<AuthorModel>> response = new ResponseModel<List<AuthorModel>>();
+            try 
+            {
+                var authors = await _context.Authors.ToListAsync();
+
+                response.Data = authors;
+                response.Message = "All authors were collected!";
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+            }
+
+            return response;
+        }
     }
 }
--- Services/Author/IAuthorInterface.cs
+++ Services/Author/IAuthorInterface.cs
@@ -1,6 +1,11 @@
-﻿namespace WebAPI_DOTNET8.Services.Author
+﻿using WebAPI_DOTNET8.Models;
+
+namespace WebAPI_DOTNET8.Services.Author
 {
     public interface IAuthorInterface
     {
+        Task<ResponseModel<List<AuthorModel>>> ListAuthors();
+        Task<ResponseModel<AuthorModel>> GetAuthorById(int idAuthor);
+        Task<ResponseModel<AuthorModel>> GetAuthorByBookId(int bookId);
     }
 }
