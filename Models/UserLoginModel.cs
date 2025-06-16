e4b94ac13fe60e0387024e13f21d9ea2a8794a43
erickbauerv <contatoerickbauer@gmail.com>
2025-06-16 17:34:24 -0300

    Implemented UserLoginModel

--- Models/UserLoginModel.cs
+++ Models/UserLoginModel.cs
@@ -3,7 +3,9 @@
     public class UserLoginModel
     {
         public int Id { get; set; }
-        public string? UserName { get; set; }
-        public string? Password { get; set; }
+        public required string UserName { get; set; }
+        public required byte[] PasswordHash { get; set; }
+        public required byte[] PasswordSalt { get; set; }
+        public string Role { get; set; } = "User";
     }
 }
