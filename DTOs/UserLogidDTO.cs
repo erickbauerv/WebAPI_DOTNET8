c799b2f7212fdbc0c3e87d472989e2fd04d57dc3
erickbauerv <contatoerickbauer@gmail.com>
2025-06-16 18:13:07 -0300

    Update UserLoginDTO

--- DTOs/UserLogidDTO.cs
+++ DTOs/UserLogidDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace WebAPI_DOTNET8.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_DOTNET8.DTOs
 {
     public class UserLoginDTO
     {
-        public string? UserName { get; set; }
-        public string? Password { get; set; }
+        [Required]
+        public required string UserName { get; set; }
+        [Required]
+        public required string Password { get; set; }
     }
 }
