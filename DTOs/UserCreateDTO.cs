a489b6379803f364e868b7041ae143d1d5a39b9c
erickbauerv <contatoerickbauer@gmail.com>
2025-06-20 18:21:01 -0300

    Implemented UserCreateDTO

---
+++ DTOs/UserCreateDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_DOTNET8.DTOs
+{
+    public class UserCreateDTO
+    {
+        [Required(ErrorMessage = "Nome de usuário é obrigatório")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Nome deve ter entre 3 e 20 caracteres")]
+        public required string UserName { get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigatória")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Senha deve ter no mínimo 8 caracteres")]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Senhas não conferem")]
+        [DataType(DataType.Password)]
+        public required string ConfirmPassword { get; set; }
+    }
+}
